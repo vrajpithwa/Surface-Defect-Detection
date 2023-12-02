c91414ac6c9d8413a35bed2ae2355e2fe45ec763
Vraj Pithwa <vraj.pithwa.in@gmail.com>
2023-12-03 00:34:33 +0530

    Optimised Random  Forest  model

---
+++ optimisedCNN.py
@@ -0,0 +1,95 @@
+import os
+import numpy as np
+import tensorflow as tf
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint
+
+# Set the seed for reproducibility
+np.random.seed(42)
+
+# Define the data directory
+data_dir = 'NEU-DET/train'
+
+# Set parameters
+input_shape = (224, 224, 3)  # Adjust dimensions based on your images
+batch_size = 32
+epochs = 20
+
+# Data augmentation and normalization
+datagen = ImageDataGenerator(
+    rescale=1./255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    validation_split=0.2  # 80% training, 20% validation
+)
+
+# Load and prepare the training dataset
+train_generator = datagen.flow_from_directory(
+    data_dir,
+    target_size=(input_shape[0], input_shape[1]),
+    batch_size=batch_size,
+    class_mode='categorical',
+    subset='training'
+)
+
+# Load and prepare the validation dataset
+validation_generator = datagen.flow_from_directory(
+    data_dir,
+    target_size=(input_shape[0], input_shape[1]),
+    batch_size=batch_size,
+    class_mode='categorical',
+    subset='validation'
+)
+
+# Build the CNN model
+model = Sequential()
+
+model.add(Conv2D(32, (3, 3), input_shape=input_shape, activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(train_generator.num_classes, activation='softmax'))
+
+# Compile the model
+model.compile(optimizer=Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Save the best model during training
+checkpoint = ModelCheckpoint('best_model.h5', save_best_only=True, monitor='val_loss', mode='min', verbose=1)
+
+# Train the model
+history = model.fit(
+    train_generator,
+    steps_per_epoch=train_generator.samples // batch_size,
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps=validation_generator.samples // batch_size,
+    callbacks=[checkpoint]
+)
+
+# Evaluate the model
+test_loss, test_acc = model.evaluate(validation_generator, verbose=2)
+print(f'Test accuracy: {test_acc}')
+
+# Optionally, you can plot training history to visualize the training process
+import matplotlib.pyplot as plt
+
+plt.plot(history.history['accuracy'], label='Training Accuracy')
+plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
